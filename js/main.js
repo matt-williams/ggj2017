5271bb169438dc319e1093f37c9d42f14c2de594
Matt Williams <matwilliams@hotmail.com>
2017-01-20 19:17:53 +0000

    Maybe better rendering

--- js/main.js
+++ js/main.js
@@ -12,12 +12,16 @@ function init() {
   camera.rotation.x = -Math.PI / 2;
   camera.position.set(0, 6, 0);
 
+  var directionalLight = new THREE.DirectionalLight(0x7f7f7f, 2);
+  directionalLight.position.set(0, 1, 0);
+  scene.add(directionalLight);
+
   var ambientLight = new THREE.AmbientLight(0x808080);
   scene.add(ambientLight);
 
   geometry = new THREE.PlaneBufferGeometry(10, 10, 50, 50);
   geometry.rotateX(-Math.PI / 2);
-  var material = new THREE.MeshBasicMaterial({color: 0xffff00, side: THREE.DoubleSide});
+  var material = new THREE.MeshBasicMaterial({color: 0xffff00, wireframe: true});
   mesh = new THREE.Mesh(geometry, material);
   mesh.position.set(0, -10, 0);
   scene.add(mesh);
@@ -34,7 +38,7 @@ function animate() {
   t++;
   var vertices = geometry.attributes.position.array;
   for (var i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3) {
-    vertices[j + 1] = Math.sin((i + t) * 0.05);
+    vertices[j + 1] = Math.sin(((i % 50) + Math.floor(i / 50) + t) * 0.5);
   }
   geometry.attributes.position.needsUpdate = true;
 
