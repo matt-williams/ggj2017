89749e64ee4f244bd00e1cbd092d5653f8ceea79
Bilen Ahmet <games.pelagic@gmail.com>
2017-01-21 02:48:57 +0000

    Adding a second scene

--- js/maps.js
+++ js/maps.js
@@ -1,5 +1,6 @@
 var maps = [
   {
+    scene: 0,
     size: {dx: 200, dz: 200},
     barriers: [
       {x: 10, z: 10, dx: 10, dz: 10}
@@ -10,5 +11,12 @@ var maps = [
     untappables: [
       {x: 20, z: 20, dx: 20, dz: 20}
     ]
+  },
+  {
+    scene: 1,
+    size: {dx: 300, dz: 100},
+    barriers: [],
+    buoys: [],
+    untappables: []
   }
 ]
